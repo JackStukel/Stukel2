de4716af582be6169dc7beea36f59f3abe8015e1
JackStukel <jack.stukel@jacks.sdstate.edu>
2026-03-10 19:15:46 -0500

    Completed Assignment 2

--- Stukel2/obj/Debug/net10.0/Stukel2.AssemblyInfo.cs
+++ Stukel2/obj/Debug/net10.0/Stukel2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Stukel2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+579041003b1e6ba350e25c00aaaf5f984d702d07")]
 [assembly: System.Reflection.AssemblyProductAttribute("Stukel2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Stukel2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
